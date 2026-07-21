c2532b2d80b699f7bab88b5b094ab76829c8458f
yeshaiahu-barak <yeshaiahu.barak@gmail.com>
2026-07-22 00:28:27 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // קובץ הגדרות מרכזי למסד הנתונים (Supabase)
 // ==========================================
 const SUPABASE_URL = 'https://amzchhmwzxzyjqhobqia.supabase.co';       
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtemNoaG13enh6eWpxaG9icWlhIiwicm9sZSI6ImFub24iLCJpYXQiOjN9.eyJwQ3JvYiAzMi5ta0pjMjNEb3pMM3I2WVp1SXZZSW1mRmVtbU5vYWcxM2VuaDY'; 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtemNoaG13enh6eWpxaG9icWlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2MjgzMDQsImV4cCI6MjEwMDIwNDMwNH0.xPQcYs9Fia6RYQye6IxihB8qviCdlWW3Yyg6ZJsnTw4'; 
 
 // יצירת מופע תקשורת גלובלי משותף
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
