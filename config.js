bb931bfee83658d94e72198ab9678c8a362d3942
yeshaiahu-barak <yeshaiahu.barak@gmail.com>
2026-07-22 01:12:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // ==========================================
 // קובץ הגדרות מרכזי למסד הנתונים (Supabase)
 // ==========================================
-const SUPABASE_URL = 'https://amzchhmwzxzyjqhobqia.supabase.co/rest/v1/';       
+const SUPABASE_URL = 'https://amzchhmwzxzyjqhobqia.supabase.co';       
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtemNoaG13enh6eWpxaG9icWlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2MjgzMDQsImV4cCI6MjEwMDIwNDMwNH0.xPQcYs9Fia6RYQye6IxihB8qviCdlWW3Yyg6ZJsnTw4';
 
 // יצירת מופע תקשורת גלובלי משותף
